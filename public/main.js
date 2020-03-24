19a3ddd3cf0453f7ca5c6fbf134e54546adbd5e2
Anjelica Bethea <anjelicabethea@gmail.com>
2020-03-23 21:13:32 -0400

    first commit

---
+++ public/main.js
@@ -0,0 +1,70 @@
+var happy = document.getElementsByClassName("fas fa-smile-beam");
+var sad = document.getElementsByClassName("fas fa-angry");
+var trash = document.getElementsByClassName("fa-trash");
+
+Array.from(happy).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const happy = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        fetch('messages', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'happy':happy
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(mad).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        const mad = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        console.log('mad')
+        fetch('mad', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg,
+            'mad':mad
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const name = this.parentNode.parentNode.childNodes[1].innerText
+        const msg = this.parentNode.parentNode.childNodes[3].innerText
+        fetch('messages', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'name': name,
+            'msg': msg
+          })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
