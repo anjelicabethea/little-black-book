f2691499a57121a7d33a997997c15deb3361720a
Anjelica Bethea <anjelicabethea@gmail.com>
2020-03-29 19:11:40 -0400

    commit

--- public/main.js
+++ public/main.js
@@ -1,18 +1,18 @@
-var happy = document.getElementsByClassName("fas fa-smile-beam");
-var sad = document.getElementsByClassName("fas fa-angry");
+var thumbUp = document.getElementsByClassName("fas fa-smile-beam");
+var thumbDown= document.getElementsByClassName("fas fa-angry");
 var trash = document.getElementsByClassName("fa-trash");
 
-Array.from(happy).forEach(function(element) {
+Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const happy = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const thumbUp = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
         fetch('messages', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
             'name': name,
             'msg': msg,
-            'happy':happy
+            'thumbUp':thumbUp
           })
         })
         .then(response => {
@@ -25,11 +25,11 @@ Array.from(happy).forEach(function(element) {
       });
 });
 
-Array.from(mad).forEach(function(element) {
+Array.from(thumbDown).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
-        const mad = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
+        const thumbDown = parseFloat(this.parentNode.parentNode.childNodes[5].innerText)
         console.log('mad')
         fetch('mad', {
           method: 'put',
@@ -37,7 +37,7 @@ Array.from(mad).forEach(function(element) {
           body: JSON.stringify({
             'name': name,
             'msg': msg,
-            'mad':mad
+            'thumbDown':thumbDown
           })
         })
         .then(response => {
